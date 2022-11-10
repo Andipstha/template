7298ed2a97d917e2c32f4a700a5bbae6f14146f5
Sandip Shrestha <52471273+Andipstha@users.noreply.github.com>
2022-11-10 18:12:58 +0000

    update about page

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sandip Shrestha",
+  title: "Graphic & Web Designer",
+  email: "sandip.stha120@gmail.com",
+  gitHub: "andipstha",
+  instagram: "andip_stha",
+  linkedIn: "sandipshrestha",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "andipstha",
+  youTube: "sandipshrestha",
 };
 
 const primaryColor = "#4E567E";
